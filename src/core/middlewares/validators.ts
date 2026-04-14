121e43f47a4e2107fd76d0e51ccf87e769efd37a
bloop <diogofalardo04@gmail.com>
2026-04-14 18:08:02 +0100

    validator to check if a user has the required permission

--- src/core/middlewares/validators.ts
+++ src/core/middlewares/validators.ts
@@ -1,6 +1,14 @@
 import { z } from "zod";
 import { HTTPException } from "hono/http-exception";
 import { HttpStatus } from "../utils/statusCode";
+import {
+  pterosRolesPermissionsServer,
+  pteroStaffServer,
+} from "../../modules/pteros/ptero.server";
+import { fa } from "zod/v4/locales";
+
+const pteroStaffService = new pteroStaffServer();
+const pterosRolesPermissionsService = new pterosRolesPermissionsServer();
 
 // generic uuid validation
 export function validateUUID(uuid: string) {
@@ -13,4 +21,30 @@ export function validateUUID(uuid: string) {
   }
 
   return uuid;
+}
+
+// validate if an user has the required permissions
+// check if user its a valid staff member
+// if its staff member collect its role
+// if that role has the required permission
+export async function validateIfUserHasRequiredPermissions(
+  userId: string,
+  pteroId: string,
+  permissionId: string,
+): Promise<boolean> {
+  const isUserAValidStaffMember = await pteroStaffService.isUserAStaffMember(
+    userId,
+    pteroId,
+  );
+  if (!isUserAValidStaffMember) return false;
+
+  const doesTheRoleHaveThePermission =
+    await pterosRolesPermissionsService.validateIfRoleIdHasPermissionId(
+      isUserAValidStaffMember.roleId,
+      permissionId,
+    );
+
+  if (!doesTheRoleHaveThePermission) return false;
+
+  return true;
 }
